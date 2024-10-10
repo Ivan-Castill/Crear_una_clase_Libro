8cd5e5df8fb1c9b96e3e6672c9400968341b56a8
Ivan-Castill <ivan2003castillo12345@gmail.com>
2024-10-09 22:42:22 -0500

    Tarea_S1_Ejercicio_2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+
+class Libro {
+    private String titulo;
+    private String autor;
+    private int paginaCount;
+    public Libro(String titulo, String autor, int paginaCount) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.paginaCount = paginaCount;
+    }
+    public String infoLibro() {
+        return "Título: " + titulo + ", Autor: " + autor + ", Páginas: " + paginaCount;
+    }
+}
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("\nLos siguientes libros estan en la biblioteca: \n");
+        Libro libro1 = new Libro("Cien años de soledad", "Gabriel García Márquez", 417);
+        Libro libro2 = new Libro("1984", "George Orwell", 328);
+        System.out.println(libro1.infoLibro());
+        System.out.println(libro2.infoLibro());
+        System.out.println("\n¡QUE TENGAN UN LINDO DIA/NOCHE!");
     }
 }
